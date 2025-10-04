be1cf7aa9fb147948d74e9f625d2b7995243a62c
Devlaxpartner <friend-gamer-123@outlook.com>
2025-10-03 20:12:18 -0700

    Update postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,8 +2,9 @@
 mkdir -p /tmp/windows-chunks
 cd /tmp/windows-chunks
 
-curl -L -O "https://github.com/Gamer-friend/windows-11-new-1/releases/download/v1.0/data.7z.001"
-curl -L -O "https://github.com/Gamer-friend/windows-11-new-1/releases/download/v1.0/data.7z.002"
+curl -L -O "https://github.com/Gamer-friend/windows-11-new-1/releases/download/v1.0/data.7z.aa"
+curl -L -O "https://github.com/Gamer-friend/windows-11-new-1/releases/download/v1.0/data.7z.ab"
+curl -L -O "https://github.com/Gamer-friend/windows-11-new-1/releases/download/v1.0/data.7z.ac"
 
 cat data.7z.* > data.7z
 7z x data.7z -o/workspaces/windows
